722f6e937b5e73cb98cbe5bf1c8a94f7c1a94bb1
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-07-31 22:18:28 +0600

    first commit

---
+++ src/page/Home.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+        <h1>We all Test Our activities here </h1>
+    </div>
+  )
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, Navigate } from "react-router-dom";
 import Login from "../page/Login";
 import Register from "../page/Register";
 import Protected from "./Protected";
+import Home from "../page/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/dashboard" replace />,
+    // element: <Navigate to="/dashboard" replace />,
+    element:<Home/>
   },
   {
     path: "/login",
