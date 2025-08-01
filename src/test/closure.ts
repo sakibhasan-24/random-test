5ffc45593a96ee4f14eba2c08e4d3d116ca56439
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-08-02 00:00:16 +0600

    flyover tool system using closure

--- src/test/closure.ts
+++ src/test/closure.ts
@@ -1,5 +1,5 @@
 // Outer Returns Inner Function
-function outer() {
+export function outer() {
   let count = 0;
   function inner() {
     count++;
@@ -13,3 +13,25 @@ console.log(increment())
 
 
 
+
+
+// let's implement a FlyOver tool system how many car passed through a bridge
+// we have a function that returns a function that returns a function
+// the first function is the bridge
+// the second function is the car
+
+export function bridge(bridgeName:string) {
+    let count=0;
+
+    // second inner function ,we will return it
+    return function(carName:string) {
+        count++;
+        return `${carName} passed over ${bridgeName} ,So far total passed${count} vehicles`;
+    }
+}
+
+const bridge1=bridge("Bridge1");
+console.log(bridge1("Car1"))
+console.log(bridge1("Car2"))
+console.log(bridge1("Car3"))
+console.log(bridge1("Car4"))
