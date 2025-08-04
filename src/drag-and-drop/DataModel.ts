30f4f86f71b806eefa540199a40bc485ea6c1b06
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-08-04 20:56:50 +0600

    a simple mock data

---
+++ src/drag-and-drop/DataModel.ts
@@ -0,0 +1,6 @@
+export interface DataModel {
+  id: number;
+  title: string;
+  type: string;
+}
+ 
---
+++ src/drag-and-drop/TaskData.ts
@@ -0,0 +1,21 @@
+import type { DataModel } from "./DataModel";
+
+export const taskData:DataModel[]=[
+    {
+        id:1,
+        title:"Task 1",
+        type:"todo"
+        
+    },
+    {
+        id:2,
+        title:"Task 2",
+        type:"todo"
+        
+    },
+    {
+        id:3,
+        title:"Task 3",
+        type:"dev"
+    }
+]
