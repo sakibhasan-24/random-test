06deadeb17aaa14541e1bb657013b411de6b574a
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-08-06 22:56:34 +0600

    data model and data

---
+++ src/transform-data/data.ts
@@ -0,0 +1,8 @@
+import type { dataModel } from "./data-model";
+
+export const dataTransform:dataModel[]=[
+     { id: 1, category: "Fruit", name: "Apple" },
+  { id: 2, category: "Fruit", name: "Banana" },
+  { id: 3, category: "Vegetable", name: "Carrot" },
+  { id: 4, category: "Vegetable", name: "Broccoli" }
+]
