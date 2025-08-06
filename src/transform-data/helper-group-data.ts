2b04c67b04fe9bc08cc9e2dca8067c289d6c091a
sakibhasan-24 <sakibhasan7724@gmail.com>
2025-08-06 23:30:04 +0600

    helper function for group data

---
+++ src/transform-data/helper-group-data.ts
@@ -0,0 +1,29 @@
+import type { dataModel } from './data-model';
+export const groupData = (data: dataModel[]) => {
+    /* 
+       [
+            {
+                category:"fruit",
+                items:["apple","banana","mango"]
+            }
+       ]
+    
+    */
+   let result: any[] = [];
+   data.forEach((item) => {
+        //find the index 
+        let index=result.findIndex((i) => i.category === item.category);
+        console.log(index);
+        //if not found then add the category and items
+        if(index === -1){
+            result.push({
+                category:item.category,
+                items:[item.name]
+            })
+        }
+        //if found then add the item to the items array
+        else{
+            result[index].items.push(item.name);
+        }
+   })
+}
